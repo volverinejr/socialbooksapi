318cca175879d90fc3babb4b102b7c5a5796a4ed
MIro <volverinejr@gmail.com>
2020-01-22 08:50:17 -0300

    testando o gitKraken

--- src/main/java/com/miro/socialbooks/domain/Livro.java
+++ src/main/java/com/miro/socialbooks/domain/Livro.java
@@ -67,51 +67,78 @@ public class Livro {
 		this.nome = nome;
 	}
 	
-	
-	
+
+
 	public Long getId() {
 		return id;
 	}
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 	public String getNome() {
 		return nome;
 	}
+
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
 	public Date getPublicacao() {
 		return publicacao;
 	}
+
+
 	public void setPublicacao(Date publicacao) {
 		this.publicacao = publicacao;
 	}
+
+
 	public String getEditora() {
 		return editora;
 	}
+
+
 	public void setEditora(String editora) {
 		this.editora = editora;
 	}
+
+
 	public String getResumo() {
 		return resumo;
 	}
+
+
 	public void setResumo(String resumo) {
 		this.resumo = resumo;
 	}
+
+
 	public List<Comentario> getComentarios() {
 		return comentarios;
 	}
+
+
 	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
 	}
+
+
 	public Autor getAutor() {
 		return autor;
 	}
+
+
 	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
 	
+
 	
 	
 }
